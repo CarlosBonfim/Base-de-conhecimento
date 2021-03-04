2608a72711f65f878df96237a6a94368ec882841
CarlosBonfim <brunobonfim10@hotmail.com>
2021-03-03 21:00:21 -0400

    correções e insersões
    criada a função para ler e montar a arvore de categorias

--- backend/api/category.js
+++ backend/api/category.js
@@ -16,7 +16,7 @@ module.exports = app => {
             app.db('categories')
                 .update(category)
                 .where({ id: category.id })
-                .then(_ => res.status(500).send(err))
+                .then(_ => res.status(204).send(err))
                 .catch(err => res.status(500).send(err))
         } else {
             app.db('categories')
@@ -35,17 +35,77 @@ module.exports = app => {
             notExistsOrError(subcategory, 'Categoria possui subcategorias.')
 
             const articles = await app.db('articles')
-                .where({categoryId: req.params.id})
+                .where({ categoryId: req.params.id })
 
             notExistsOrError(articles, 'Essa categoria possui artigos inseridos')
 
             const rowsDeleted = await app.db('categories')
-                .where({id: req.params.id}).del()
-            existsOrError(rowsDeleted, 'Categoria nao inforamda.')    
+                .where({ id: req.params.id }).del()
+            existsOrError(rowsDeleted, 'Categoria não foi encontrada.')
             res.status(204).send()
-        }catch(msg){
+        } catch (msg) {
             res.status(400).send(msg)
         }
     }
 
+    const withPath = categories => {
+        const getParent = (categories, parentId) => {
+            const parent = categories.filter(parent => parent.id === parentId)
+            return parent.length ? parent[0] : null
+        }
+
+        const categoriesWithPath = categories.map(category => {
+            let path = category.name
+            let parent = getParent(categories, category.parentId)
+
+            while (parent) {
+                path = `${parent.name} > ${path}`
+                parent = getParent(categories, parent.parentId)
+            }
+
+            return { ...category, path }
+        })
+
+        categoriesWithPath.sort((a, b) => {
+            if (a.path < b.path) return -1
+            if (a.path > b.path) return 1
+            return 0
+        })
+        return categoriesWithPath
+
+    }
+    const get = (req, res) => {
+        app.db('categories')
+            .then(categories => res.json(withPath(categories)))
+            .catch(err => res.status(500).send(err))
+
+    }
+
+    const getById = (req, res) => {
+        app.db('categories')
+            .where({ id: req.params.id })
+            .first()
+            .then(category => res.json(category))
+            .catch(err => res.status(500).send(err))
+    }
+
+    const toTree = (categories, tree) => {
+        if(!tree) tree = categories.filter(c => !c.parentId)
+        tree = tree.map(parentNode => {
+            const isChild = node => node.parentId == parentNode.id
+            parentNode.children = toTree(categories, categories.filter(isChild))
+            return parentNode
+        })
+        return tree
+    }
+
+    const getTree = (req,res) => {
+        app.db('categories')
+            .then(categories => res.json(toTree(categories)))
+            .catch(err => res.status(500).msg(err))
+    }
+
+    
+
+    return { save, remove, get, getById, getTree }
 }
