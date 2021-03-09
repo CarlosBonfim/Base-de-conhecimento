20295874d5ed7779c20d5a62e53d048e3c0cbeec
CarlosBonfim <brunobonfim10@hotmail.com>
2021-03-08 20:07:00 -0400

    Update user.js
    Adicionadas as funções para fazer um soft delete do usuario

--- backend/api/user.js
+++ backend/api/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt-nodejs')
+const { reset } = require('nodemon')
 
 module.exports = app => {
     const { existsOrError, notExistsOrError, equalsOrError } = app.api.validation
@@ -36,6 +37,7 @@ module.exports = app => {
             app.db('users')
                 .update(user)
                 .where({ id: user.id })
+                .whereNull('deletedAt')
                 .then(_ => res.status(204).send())
                 .catch(err => res.status(500).send(err))
         } else {
@@ -48,6 +50,7 @@ module.exports = app => {
     const get = (req, res) => {
         app.db('users')
             .select('id', 'name', 'email', 'admin')
+            .whereNull('deletedAt')
             .then(user => res.json(user))
             .catch(err => res.status(500).send(err))
     }
@@ -56,10 +59,27 @@ module.exports = app => {
         app.db('users')
             .select('id', 'name', 'email', 'admin')
             .where({ id: req.params.id })
+            .whereNull('deletedAt')
             .first()
             .then(use => res.json(use))
             .catch(err => res.status(500).send(err))
     }
 
-    return { save, get, getById }
+    const remove = async (req,res) => {
+        try {
+            const articles = await app.db('articles')
+                .where({userId: req.params.id})
+            notExistsOrError(articles, 'Usuário possui artigos.')
+
+            const rowsUpdated = await app.db('users')
+                .update({deletedAt: new Date()})
+                .where({id: req.params.id})
+            existsOrError(rowsUpdated, 'Usuário não foi encontrado.')
+            res.status(204).send()
+        }catch(msg){
+            res.status(400).send(msg)
+        }
+    }
+
+    return { save, get, getById, remove }
 }
