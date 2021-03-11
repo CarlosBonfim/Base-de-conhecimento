fd177ed1ad2df09ebf55b79202ac7be1a5cd9e59
CarlosBonfim <brunobonfim10@hotmail.com>
2021-03-10 20:06:39 -0400

    Update App.vue
    Inicio do frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,25 @@
 <template>
 	<div id="app">
-		<h1>Versão Inicial</h1>
+		<Header />
+		<Menu />
+		<Content/>
+		<Footer />
 	</div>
 </template>
 
 <script>
+import Header from "@/components/template/Header"
+import Menu from "@/components/template/Menu"
+import Content from "@/components/template/Content"
+import Footer from "@/components/template/Footer"
+
+
 export default {
 	name: "App",
+	components: { Header, Menu, Content, Footer}
 }
 </script>
+
 
 <style>
 
