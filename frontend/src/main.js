705f0d05f27b708cbc3d10c41b84fc77bffdc411
CarlosBonfim <brunobonfim10@hotmail.com>
2021-04-01 19:26:30 -0400

    inserção dos componentes e alteração do token de sessão

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,7 +7,7 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 // temporario
-require('axios').defaults.headers.common['Authorization'] = ' bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkJydW5vIiwiZW1haWwiOiJicnVub0BlbWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNjE2MTc4NTgwLCJleHAiOjE2MTY0Mzc3ODB9.AQcqoIaN9ljyYLNye6CUvyPAWkpyU3vKYV56_QZSG-0'
+require('axios').defaults.headers.common['Authorization'] = ' bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkJydW5vIiwiZW1haWwiOiJicnVub0BlbWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNjE3MDQ3OTg0LCJleHAiOjE2MTczMDcxODR9.Ew6uNDPWPxZ12pVhcLsc0yFpKPkwPDzy1tnqRidc-HY'
 
 new Vue({
   store,
