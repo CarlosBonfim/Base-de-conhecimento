9bd62519b99ee4719b2dcaf6a12b4bae7128e177
CarlosBonfim <brunobonfim10@hotmail.com>
2021-04-02 20:48:50 -0400

    Update CategoryAdmin.vue
    Adição da tabela de exibição das categorias

--- frontend/src/components/admin/CategoryAdmin.vue
+++ frontend/src/components/admin/CategoryAdmin.vue
@@ -1,12 +1,39 @@
 <template>
   <div class="category-admin">
-      <h1>Categoria Componente</h1>
+      <b-table hover striped :items="this.categories" :fields="fields"></b-table>
   </div>
 </template>
 
 <script>
+import {baseApiUrl, showError} from '@/global'
+import axios from 'axios'
 export default {
-    name:'CategoryAdmin'
+    name:'CategoryAdmin',
+    data: function() {
+      return{
+        mode: 'save',
+        category: {},
+        categories: [],
+        fields:[
+          {key: 'id', label: 'Código', sortable: true},
+          {key: 'name', label: 'Nome', sortable: true},
+          {key:'parentId', label: 'Categoria Pai', sortable: true},
+          {key: 'path', label: 'Caminho', sortable: true},
+          {key: 'actions', label: 'Ações', sortable: true}
+        ]
+      }
+    },
+    methods: {
+      loadCategories(){
+        const url = `${baseApiUrl}/categories`
+        axios.get(url).then(res => {
+          this.categories = res.data
+        })
+      }
+    },
+    mounted(){
+      this.loadCategories()
+    }
 }
 </script>
 
