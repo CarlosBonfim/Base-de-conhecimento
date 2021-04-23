412c7606f5ef3cead8ede903ba853c9111f31ada
CarlosBonfim <brunobonfim10@hotmail.com>
2021-04-23 15:01:59 -0400

    Create ArticlesByCategory.vue
    criado articlesbycategory para exibição dos artigos com base na categoria

---
+++ frontend/src/components/article/ArticlesByCategory.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="articles-by-category">
+        <PageTitle icon="fa fa-folder-o" :main="category.name" sub="Categoria" />
+    </div>
+</template>
+
+<script>
+import { baseApiUrl } from "@/global";
+import axios from "axios";
+import PageTitle from ".../template/PageTitle";
+
+export default {
+  name: "ArticlesByCategories",
+  components: { PageTitle },
+  data: function () {
+    return { 
+        category: {}, 
+        articles: [], 
+        page: 1, 
+        loadMore: true
+    };
+  },
+  methods: {
+        getCategory(){
+            const url = `${baseApiUrl}/categories/${this.category.id}`
+            axios(url)
+                .then(res => this.category = res.data)
+        }
+    }
+};
+</script>
+
+<style>
+</style>
