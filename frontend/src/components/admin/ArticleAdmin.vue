41aae9739c94ca28b42b2b9b4a6c70c64a0a450f
CarlosBonfim <brunobonfim10@hotmail.com>
2021-04-23 13:54:28 -0400

    Update ArticleAdmin.vue
    Foi autalizado o componente de artigo, com a iserção da paginação da tabela de artigos.

--- frontend/src/components/admin/ArticleAdmin.vue
+++ frontend/src/components/admin/ArticleAdmin.vue
@@ -40,6 +40,7 @@
         </b-button>
       </template>
     </b-table>
+    <b-pagination size="md" v-model="page" :total-rows="count" :per-page="limit" ></b-pagination>
   </div>
 </template>
 
@@ -73,7 +74,7 @@ export default {
   },
   methods: {
     loadArticles() {
-      const url = `${baseApiUrl}/articles`;
+      const url = `${baseApiUrl}/articles?page=${this.page}`;
       axios.get(url).then((res) => {
         //this.articles = res.data
         this.articles = res.data.data
@@ -127,6 +128,11 @@ export default {
                 return {value: user.id, text: `${user.name} - ${user.email}`}
             })
         })
+    }
+  },
+  watch:{
+    page(){
+        this.loadArticles()
     }
   },
 
