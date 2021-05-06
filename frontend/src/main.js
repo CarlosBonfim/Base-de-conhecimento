43d99ea3dfb1cded2c4601fec39efd92dbbdd49d
CarlosBonfim <brunobonfim10@hotmail.com>
2021-05-06 16:21:53 -0400

    Update main.js
    importado o arquivo de verificação de sessão

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,13 @@
 import 'font-awesome/css/font-awesome.css'
 import Vue from 'vue'
 import App from './App'
-import store from './config/store'
+import './config/axios'
 import './config/bootstrap'
-import router from './config/router'
 import './config/msgs'
+import store from './config/store'
+import router from './config/router'
+
+
 
 Vue.config.productionTip = false
 // temporario
