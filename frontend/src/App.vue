6dd0666490a8675cba8c22b6e7736576149db44a
CarlosBonfim <brunobonfim10@hotmail.com>
2021-05-06 14:22:28 -0400

    criado o componente de validação do token e exibição da tela de loading

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,23 +2,62 @@
 	<div id="app" :class="{'hide-menu': !isMenuVisible || !user}">
 		<Header title="Base de conhecimento" :hideToogle="!user" :hideUserDropdown="!user"/>
 		<Menu v-if="user"/>
-		<Content/>
+		<Loading v-if="validatingToken"/>
+		<Content v-else/>
+		<!-- <Content/> -->
 		<Footer />
 	</div>
 </template>
 
 <script>
+import axios from 'axios'
+import {baseApiUrl, userKey} from '@/global'
 import {mapState} from 'vuex'
 import Header from "@/components/template/Header"
 import Menu from "@/components/template/Menu"
 import Content from "@/components/template/Content"
 import Footer from "@/components/template/Footer"
+import Loading from '@/components/template/Loading'
 
 
 export default {
 	name: "App",
-	components: { Header, Menu, Content, Footer},
-	computed: mapState(['isMenuVisible', 'user'])//monitora o estado em store.js
+	components: { Header, Menu, Content, Footer, Loading},
+	computed: mapState(['isMenuVisible', 'user']),//monitora o estado em store.js
+	data: function(){
+		return{
+			validatingToken: true,
+
+		}
+	},
+	methods:{
+		async validateToken(){
+			this.validatingToken = true
+
+			const json = localStorage.getItem(userKey)
+			const userData = JSON.parse(json)
+			this.$store.commit('setUser', null)
+
+			if(!userData){
+				this.validatingToken = false
+				this.$router.push({name: 'auth'})
+				return
+			}
+
+			const res = await axios.post(`${baseApiUrl}/validateToken`, userData)
+
+			if(res.data){
+				this.$store.commit('setUser', userData)
+			} else{
+				localStorage.removeItem(userKey)
+				this.$router.push({name: 'auth'})
+			}
+			this.validatingToken = false
+		}
+	},
+	created(){
+		this.validateToken()
+	}
 }
 </script>
 
